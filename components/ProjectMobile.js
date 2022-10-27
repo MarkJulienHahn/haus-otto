8aaddb411a608a51f926a805ce338b364f02625f
Mark Julien Hahn <mail@markjulienhahn.de>
2022-10-27 18:15:59 +0200

    ...

--- components/ProjectMobile.js
+++ components/ProjectMobile.js
@@ -50,6 +50,7 @@ const ProjectMobile = ({
     index === activeIndex ? setActive(true) : setActive(false);
   }, [activeIndex]);
 
+  useEffect(() => scrollUp() )
 
   const archiveAction = (indx) => {
     setSwiperINDX(indx), setShowIndex(false);
