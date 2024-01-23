15d1ba62c6b8f92de6e62921bf08c37c7aceed43
Mark Julien Hahn <mail@markjulienhahn.de>
2024-01-23 11:35:19 +0100

    Troubleshoot Mobile

--- components/Projects.js
+++ components/Projects.js
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect } from "react";
 
 import styles from "../styles/project.module.css";
 
-import useWindowDimensions from './hooks/useWindowDimensions';
+import useWindowDimensions from "./hooks/useWindowDimensions";
 
 import Project from "./Project";
 import ProjectMobile from "./ProjectMobile";
@@ -10,7 +10,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 const Projects = ({ projects, data }) => {
-  const { height } = useWindowDimensions();
+  const { width, height } = useWindowDimensions();
 
   const [activeIndex, setActiveIndex] = useState(null);
   const [refHeight, setRefHeight] = useState();
@@ -31,36 +31,48 @@ const Projects = ({ projects, data }) => {
         setActiveIndex={setActiveIndex}
         data={data}
       />
-      <div
-        className={styles.projectsWrapper}
-        ref={projectsRef}
-        style={
-          activeIndex == null
-            ? { position: "absolute", top: `calc(100vh - ${refHeight + 39}px)` }
-            : { position: "absolute", top: `0px` }
-        }
-      >
-        <div className={styles.projectsInner} style={!activeIndex ? {paddingTop: refHeight - height + 40} : {paddingTop: 0}}>
-          {projects.map((project, i) => (
-            <Project
-              key={i}
-              setActiveIndex={setActiveIndex}
-              activeIndex={activeIndex}
-              title={project.title}
-              category={project.case}
-              client={project.client}
-              photography={project.photography}
-              presskit={project.presskit?.url}
-              description={project.description}
-              year={project.year}
-              index={i}
-              images={project.images}
-              previewImage={project.previewImage}
-            />
-          ))}
+      {width > 700 && (
+        <div
+          className={styles.projectsWrapper}
+          ref={projectsRef}
+          style={
+            activeIndex == null
+              ? {
+                  position: "absolute",
+                  top: `calc(100vh - ${refHeight + 39}px)`,
+                }
+              : { position: "absolute", top: `0px` }
+          }
+        >
+          <div
+            className={styles.projectsInner}
+            style={
+              !activeIndex
+                ? { paddingTop: refHeight - height + 40 }
+                : { paddingTop: 0 }
+            }
+          >
+            {projects.map((project, i) => (
+              <Project
+                key={i}
+                setActiveIndex={setActiveIndex}
+                activeIndex={activeIndex}
+                title={project.title}
+                category={project.case}
+                client={project.client}
+                photography={project.photography}
+                presskit={project.presskit?.url}
+                description={project.description}
+                year={project.year}
+                index={i}
+                images={project.images}
+                previewImage={project.previewImage}
+              />
+            ))}
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      )}
 
       <div
         className={styles.projectsMobileWrapper}
